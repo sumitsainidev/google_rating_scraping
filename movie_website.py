8f400a76fd5e08a4042c5c9e346a3c38bc5db6e2
Sumit Saini <sumitsaini199711@gmail.com>
2024-10-18 21:43:47 +0530

    updated

--- movie_website.py
+++ movie_website.py
@@ -7,6 +7,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import re
 chrome_options = Options()
 ia = imdb.Cinemagoer()
 # top = ia.get_top50_movies_by_genres('sci-fi')
@@ -18,39 +20,70 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 name = []
 googleRating = []
+userNoRating = []
 
 tempA = []
-for i in range(50):
+for i in range(0,50):
     page_number = i + 1
-    url = f"https://bolly4u.studio/page/{page_number}/"
+    url = f"https://bolly4u.tech/page/{page_number}/"
 
 
     response = requests.get(url)
-    parsed_data = json.loads(response.text)
-    print(parsed_data)
-    for i in parsed_data['results']:
+    parsed_data = response.text
+
+    soup = BeautifulSoup(parsed_data, 'html.parser')
+    div_elements = soup.find_all('div', class_='mt-2 transition-all block px-2 py-3 shadow-inner text-gray-500 group-hover:text-indigo-500 tracking-wide text-sm')
+    text_contents = [div.get_text(strip=True) for div in div_elements]
+
+    movies_arr = []
+
+    for text_content in text_contents:
+        pattern = r'^[^\d]+ \d{4}'
+        matches = re.findall(pattern, text_content, re.MULTILINE)
+        if len(matches)>0:
+            movies_arr.append(matches[0])
+
+    for i in movies_arr:
     # for i in tempA:
         try:
-            print('checking..',i['title'])
-            url = 'https://www.google.com/search?q='+i['title']+' movie'
+            # print('checking..',i)
+            url = 'https://www.google.com/search?q='+i+' movie reviews'
             driver.get(url)
             
-            h1 = driver.find_element_by_xpath("//*[contains(text(), 'liked this film')]")
+            h1 = driver.find_element(By.XPATH,"//*[contains(text(), 'liked this film')]")
+            numOfUserRatings  = 0
+            # try:
+            #     film_review_element = driver.find_element_by_xpath('//*[@data-ti="FilmReview"]')
+            #     film_review_element.click()
+            #     print("Element found and clicked.")
+            # except e:
+            #     pass
+
+            try:
+                # Find the div element with the class "H5xxEd" that contains the word "ratings"
+                ratings_element = driver.find_element(By.XPATH, '//div[contains(text(), "ratings")]')
+    
+                # If found, you can perform further actions with this element
+                # print("Element found:", ratings_element.text)
+                numOfUserRatings = ratings_element.text
+            except e:
+                b='1'
+
             likedString = h1.text.replace('%','')
-            print(i['title'],likedString)
             res = [int(i) for i in likedString.split() if i.isdigit()]
             # print(res)
-            name.append(i['title'])
+            name.append(i)
             googleRating.append(res[0])
+            userNoRating.append(numOfUserRatings)
+            print(i,likedString,numOfUserRatings)
         except Exception as e:
-            print("An exception occurred")
+            # print("An exception occurred")
             try:
                 h2 = driver.current_url
                 if 'sorry/index?' in h2:
                     x = input("Waiting for manual date to be entered. Enter YES when done.")
             except:
-                print("Text not found on the page.")
-            continue
+                a ='1'
             
 
 zipped_lists = zip(googleRating,name)
